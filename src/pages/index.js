f21fdc8f1f025a92598b25125bb7267eae8d60fd
m.kurahara <kuraharamasahiro@gmail.com>
2020-09-24 00:44:43 +0900

    ファイル整理・ブログページ作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Kv from "../components/kv"
+import BlogItem from "../components/BlogItem"
+import { Container, Row, Col} from 'react-bootstrap';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Kv />
+    <Container>
+      <Row>
+        <Col sm={4}>
+          <BlogItem />
+        </Col>
+        <Col sm={4}>
+          <BlogItem />
+        </Col>
+        <Col sm={4}>
+          <BlogItem />
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
